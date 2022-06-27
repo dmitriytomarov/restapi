4207f1a0e37e114ec2e717fcd5da84261ac77c64
Dmitriy Tomarov <dm14@inbox.ru>
2022-06-27 17:22:11 +0300

    Добавил описание в заказы

--- RestApi/Controllers/OrdersController.cs
+++ RestApi/Controllers/OrdersController.cs
@@ -14,11 +14,6 @@ namespace RestApi.Controllers
     /*
 
 
-3)	Метод получения списка заказов по конкретному клиенту за выбранный временной период, отсортированный по дате создания.
-
-    get
-orders / clientId=          сортировка по дате
-orders / client id= date from= date to=          сортировка по дате
 
 4)	Метод формирования заказа с проверкой наличия требуемого количества товара на складе, а также уменьшение доступного количества товара на складе в БД в случае успешного создания заказа. 
 post
@@ -37,8 +32,14 @@ orders  товар/количество товар / количство това
         public OrdersController(ShopContext context) { _context = context; }
 
         /// <summary>
-        /// Получение списка всех заказов по клиенту (по Id клиента)
+        /// Получение списка всех заказов по клиенту с возможностью фильтрации по дате
         /// </summary>
+        /// <remarks>
+        /// Параметры <br/>
+        /// id клиента  (int).<br/>
+        /// dateFrom (опционально) - дата в формате dd.mm.YYYY или YYYY.mm.dd<br/>
+        /// dateTo (опционально) - дата в формате dd.mm.YYYY или YYYY.mm.dd
+        /// </remarks>
         /// <returns></returns>
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -55,7 +56,6 @@ orders  товар/количество товар / количство това
             if (dateFrom == null) dateFrom = DateTime.MinValue;
 
             return orders.Where(e => e.OrderDate >= dateFrom).ToList();
-
         }
 
 
