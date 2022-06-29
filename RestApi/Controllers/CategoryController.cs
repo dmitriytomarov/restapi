2e61fc58807dd60ac930d5f3af3588ceb5ab72e8
Dmitriy Tomarov <dm14@inbox.ru>
2022-06-29 14:10:03 +0300

    Добавил кеширование для категорий

--- RestApi/Controllers/CategoryController.cs
+++ RestApi/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using RestApi.Models;
 
 namespace RestApi.Controllers
@@ -10,7 +11,14 @@ namespace RestApi.Controllers
     public class CategoryController : Controller
     {
         private readonly ShopContext _context;
-        public CategoryController(ShopContext context) { _context = context; }
+        private readonly IMemoryCache _cash;
+        public CategoryController(ShopContext context, IMemoryCache cache)
+        {
+            _context = context;
+            _cash = cache;
+        }
+
+
 
         /// <summary>
         /// Получение списка всех категорий
@@ -19,7 +27,14 @@ namespace RestApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Category>>> GetCategories() 
         {
-            return await _context.Categories.ToListAsync();
+            List<Category> categories = new();
+            if (!_cash.TryGetValue("categories", out categories))
+            {
+                categories= await _context.Categories.ToListAsync();
+                _cash.Set("categories", categories, new MemoryCacheEntryOptions {AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10) });
+            }
+
+            return categories;
         }
     }
 }
--- RestApi/Program.cs
+++ RestApi/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 //builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<ShopContext>(opt => opt.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog=Shop231730; Integrated Security = True;"));
+builder.Services.AddMemoryCache();
 
 builder.Services.AddSwaggerGen(config =>
     {
